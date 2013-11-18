5b1d14f5b6439cdbae08a6a3a07668128247ca28
sacchan86 <sacchan86@hotmail.com>
2013-11-17 17:09:21 -1000

    roll-a-ball unity

---
+++ Assets/Scripts/PlayerController.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+var speed : float;
+var countText : GUIText;
+var winText : GUIText;
+private var count : int;
+
+function Start() {
+	count = 0;
+	SetCountText();
+	winText.text = "";
+}
+
+function FixedUpdate () {
+
+	var moveHorizontal : float = Input.GetAxis("Horizontal");
+	var moveVertical : float = Input.GetAxis("Vertical");
+	
+	var movement : Vector3 = new Vector3(moveHorizontal, 0.0f, moveVertical);
+	rigidbody.AddForce(movement * speed *Time.deltaTime);
+}
+
+function OnTriggerEnter (other : Collider) {
+	if(other.gameObject.tag == "PickUp") {
+		other.gameObject.SetActive(false);
+		count += 1;
+		SetCountText();
+	};
+}
+
+function SetCountText() {
+	countText.text = "Count: " + count;
+	if(count >= 12) {
+		winText.text = "YOU WIN!!!!!";
+	}
+}
